8dcd061ac16f510ac66d726d6c6cb78dab3948ec
soulcm <soulcm@soulcm.local>
2015-08-05 23:03:58 +0800

    环境搭建及小黄人body部分

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # minions
-参照看到的一个[小黄人](https://github.com/JR93/littleHuang)案例，学习sass和巩固css知识。
+参照最近看到的一个[小黄人](https://github.com/JR93/littleHuang)案例，学习sass和巩固css知识。
+
+
+CSS文件夹下是已编译好的CSS文件。  
+确保电脑已安装nodejs与npm工具，运行npm install，然后执行gulp sass对SASS文件夹下面的文件进行编译。  
+gulp插件gulp-sass的安装，需要Python环境，请自行网上查找Python安装方法。
---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+	gulp.src('./sass/*.scss')
+		.pipe(sass({
+			outputStyle: 'expanded'
+		}).on('error', sass.logError))
+		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function() {
+	gulp.watch('./sass/*.scss', ['sass']);
+});
